b1ad6bbc22bdfa907cf435e78a1d0bd638ae5d28
D. Andrei Măceș <dmaces@nd.edu>
2016-09-11 23:08:26 -0400

    Revert "camera: Use project pathmap"
    This reverts commit 967bff4444615598da22ebd581b166ba09b15311.
    
    Change-Id: I31d2f79ca8f03fc9e4be2c6d072d98d412892525

--- camera/QCamera/HAL/core/Android.mk
+++ camera/QCamera/HAL/core/Android.mk
@@ -64,9 +64,9 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-display)/libgenlock \
-        $(call project-path-for,qcom-media)/libstagefrighthw
+LOCAL_C_INCLUDES += hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libgralloc \
+        hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libgenlock \
+        hardware/qcom/media/$(TARGET_BOARD_PLATFORM)/libstagefrighthw
 
 # if debug service layer and up , use stub camera!
 LOCAL_C_INCLUDES += \
